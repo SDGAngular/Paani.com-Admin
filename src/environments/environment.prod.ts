675ee33ea951f17bc95e865f691aa0f54dfa4f6b
Shubhayu Dasgupta <shubhayudasgupta@Shubhayus-MacBook-Air.local>
2022-06-21 00:02:03 +0530

    environemnt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCFjZfJ_GL1jmxIFiCMmNOwCOUMIZSH580",
+  authDomain: "paani-admin.firebaseapp.com",
+  projectId: "paani-admin",
+  storageBucket: "paani-admin.appspot.com",
+  messagingSenderId: "1067954316057",
+  appId: "1:1067954316057:web:66c27281ebc6242708db88",
+  measurementId: "G-91LXF0JL0C"
+  }
 };
